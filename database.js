acc5f104a39a4146ac0aecb00c0ff4b60b644792
acantu9 <adrianaacantu@gmail.com>
2023-12-30 14:44:41 -0600

    created mysql connection, specify connection details, & export

---
+++ database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+ host: 'localhost',
+ port: 3306,
+ user: 'admin',
+ password: 'S3cur3P4sSw0rD',
+ database: 'employee_db'
+});
+
+connection.connect((err) => {
+ if (err) throw err;
+ console.log('Connected to the MySQL server.');
+});
+
+module.exports = connection;
