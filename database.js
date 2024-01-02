47f2dd7f8a551d2722648ad10d22397519f0dbf7
acantu <adrianaacantu@gmail.com>
2024-01-01 21:11:37 -0600

    updated mysql const and password in connection const

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
  host: 'localhost',
  port: 3306,
  user: 'root',
- password: 'S3cur3P4sSw0rD',
+ password: 'rootroot',
  database: 'employee_db'
 });
 
