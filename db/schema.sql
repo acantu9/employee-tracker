b32f0c7ab71af16226bc891f372e2192f88edd9d
acantu <adrianaacantu@gmail.com>
2024-01-01 22:09:04 -0600

    refractored employees table and added code to drop the foreign key constraints and the tables if they exist

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
+-- Drop the foreign key constraints
+ALTER TABLE roles DROP FOREIGN KEY roles_ibfk_1;
+ALTER TABLE employees DROP FOREIGN KEY employees_ibfk_1;
+
+-- Drop the tables if they exist
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+
 -- Create the departments table
 CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,7 +29,10 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
+  department_id INT,
+  salary DECIMAL(10, 2),
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (department_id) REFERENCES departments(id),
   FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
